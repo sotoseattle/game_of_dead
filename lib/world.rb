27b11f42ead065845d610627ce401468e198df90
Javier Soto <sotoseattle@gmail.com>
2015-05-21 15:41:32 -0700

    contagion between zombies and humans based on contact

--- lib/world.rb
+++ lib/world.rb
@@ -1,14 +1,15 @@
 require 'gosu'
 
 class Being < Gosu::Image
+  attr_reader :x, :y, :a
   VELO = 4
 
-  def initialize board, file
+  def initialize board, file, opts={}
     super board, file, false
     @board = board
-    @x = rand(0..@board.width)
-    @y = rand(0..@board.height)
-    @a = rand(0..360)
+    @x = opts[:x] || rand(0...@board.width)
+    @y = opts[:y] || rand(0...@board.height)
+    @a = opts[:a] || rand(0...360)
     @wide = 8
     @high = 8
   end
@@ -35,38 +36,54 @@ class Being < Gosu::Image
   def draw
     super(@x - @wide/2, @y - @high/2, 1)
   end
+
+  def position
+    [@x, @y]
+  end
 end
 
 class Human < Being
+  CONTACT = 8
+
   def initialize board
     super board, 'human.png'
+  end
+
+  def mutate
+    # oh noooooo!
   end
 end
 
 class Zombi < Being
-  def initialize board
-    super board, 'zombi.png'
+  def initialize board, opts = {}
+    super board, 'zombi.png', opts
   end
 end
 
 class WhackARuby < Gosu::Window
   def initialize
-    super 400, 400, false
-    self.caption = "Whack the Ruby!"
-    @human = Human.new(self)
-    @zombi = Zombi.new(self)
+    super 200, 200, false
+    self.caption = "Game of Undead"
+    @humans = Array.new(20){ Human.new(self) }
+    @zombis = Array.new(1) { Zombi.new(self) }
  end
 
   def update
-    @human.update
-    @zombi.update
+    @humans.each(&:update)
+    @zombis.each(&:update)
+
+    new_zombis, survivors = @humans.partition do |h|
+      @zombis.find{|z| Gosu.distance(*h.position, *z.position) < Human::CONTACT}
+    end
+    @humans = survivors
+    @zombis += new_zombis.map { |z| Zombi.new(self, {x:z.x, y:z.y, a:z.a}) }
   end
 
   def draw
     c = Gosu::Color::NONE
     #draw_quad(0,0,c,800,0,c,800,600,c,0,600,c)
-    @human.draw
-    @zombi.draw
+    @humans.each(&:draw)
+    @zombis.each(&:draw)
   end
 end
 
