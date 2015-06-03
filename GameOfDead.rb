d969510b4366177933006eb7647de66829d5222d
Javier Soto <sotoseattle@gmail.com>
2015-06-02 17:05:30 -0700

    wip bouncing balls

--- GameOfDead.rb
+++ GameOfDead.rb
@@ -2,6 +2,23 @@
 
 require './lib/world.rb'
 
-window = GameOfDead.new 20
+window = GameOfDead.new
+
+colors = [Gosu::Color::WHITE,
+          Gosu::Color::AQUA,
+          Gosu::Color::RED,
+          Gosu::Color::GREEN,
+          Gosu::Color::YELLOW,
+          Gosu::Color::FUCHSIA,
+          Gosu::Color::CYAN]
+3.times do
+  (0...7).each do |i|
+    b = Ball.new(window, nil)
+    b.col = colors[i]
+    window << b
+  end
+end
+
+window.ready
 
 window.show
--- lib/world.rb
+++ lib/world.rb
@@ -36,68 +36,85 @@ class Vector
     new_m = Gosu.distance(@x, @y, new_x, new_y)
     Vector.new x: @x, y: @y, a: new_a, m: new_m
   end
+
+  def - v2
+    v2.ang = -v2.ang
+    self + v2
+  end
 end
 
 class Ball < Gosu::Image
-  attr_reader :vecto
-  DRAG = 1.8
+  attr_reader :vec
+  attr_accessor :other, :col
+  DRAG = 0.8
   GRAV = Vector.new a: 90, m: 0.8
 
-  def initialize board, opts={}
+  def initialize board, veco
     super board, 'lib/ball.png', false
-    @wide = @high = 8
-    @vecto = Vector.new x: rand(0...board.width),
-                        y: rand(0...board.height),
-                        m: 4
-    @border = { south: Vector.new(y: board.height, a: 270),
-              north: Vector.new(y: 0, a: 90),
-              east:  Vector.new(x: 0, a: 0),
-              west:  Vector.new(x: board.width, a: 180) }
+    @col = Gosu::Color::WHITE
+    @len = 8
+    @vec = veco || Vector.new(x: rand(0...board.width),
+                              y: rand(0...board.height),
+                              a: rand(0...360),
+                              m: 1)
+    @other = board.balls - [self]
+    @limit = { south: Vector.new(y: board.height, a: 270),
+               north: Vector.new(y: 0, a: 90),
+               east:  Vector.new(x: 0, a: 0),
+               west:  Vector.new(x: board.width, a: 180) }
   end
 
   def update
-    apply_gravity
+    # apply_gravity
+    check_collisions
     confine_inside
-    @vecto.start_point = @vecto.end_point
+    @vec.start_point = @vec.end_point
   end
 
   def apply_gravity
-    @vecto = @vecto + GRAV
+    @vec += GRAV
+  end
+
+  def check_collisions
+    near = @other.min_by { |b| Gosu.distance(@vec.x, @vec.y, b.vec.x, b.vec.y) }
+    if near
+      if Gosu.distance(@vec.x, @vec.y, near.vec.x, near.vec.y) < @len/2
+        w = near.vec.dup
+        w.mag *= DRAG
+        @vec -= w
+      end
+    end
   end
 
   def confine_inside
-    ex, ey = @vecto.end_point
-    outside_x_limits = [ex < 0, ex > @border[:west].x]
-    outside_y_limits = [ey < 0, ey > @border[:south].y]
+    ex, ey = @vec.end_point
+    outside_x_limits = [ex < 0, ex > @limit[:west].x]
+    outside_y_limits = [ey < 0, ey > @limit[:south].y]
 
     return if (outside_x_limits + outside_y_limits).none?
 
     walls = []
     if outside_x_limits.any?
-      w = outside_x_limits.first ? @border[:east].dup : @border[:west].dup
-      w.mag = DRAG * @vecto.comp_x.abs
+      w = outside_x_limits.first ? @limit[:east].dup : @limit[:west].dup
+      w.mag = DRAG * 2 * @vec.comp_x.abs
       walls << w
     end
 
     if outside_y_limits.any?
-      w = outside_y_limits.first ? @border[:north].dup : @border[:south].dup
-      w.mag = DRAG * @vecto.comp_y.abs
+      w = outside_y_limits.first ? @limit[:north].dup : @limit[:south].dup
+      w.mag = DRAG *  2 * @vec.comp_y.abs
       walls << w
     end
 
-    bounce walls
-  end
-
-  def bounce vectors
-    vectors.each do |w|
-      w.x ||= @vecto.x
-      w.y ||= @vecto.y
-      @vecto += w
+    walls.each do |w|
+      w.x ||= @vec.x
+      w.y ||= @vec.y
+      @vec += w
     end
   end
 
   def draw
-    super(@vecto.x - @wide/2, @vecto.y - @high/2, 1)
+    super(@vec.x - @len/2, @vec.y - @len/2, 1, 1,1, color=@col)
   end
 end
 
@@ -105,11 +122,19 @@ class GameOfDead < Gosu::Window
   attr_reader :balls
   SCREEN_WIDE, SCREEN_HIGH = 500, 600
 
-  def initialize n=1
+  def initialize
     super SCREEN_WIDE, SCREEN_HIGH, false
     self.caption = "Freeaaaaakkyy!!"
-    @balls = Array.new(n){ Ball.new(self) }
+    @balls = []
     @font = Gosu::Font.new(self, 'system', 30)
+  end
+
+  def << ball
+    @balls << ball
+  end
+
+  def ready
+    @balls.each { |b| b.other = @balls - [b] }
   end
 
   def update
